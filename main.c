6fceca1e92902cbac047dfb4ce4d2a2a2fdfa621
FoundIzalith <b17kaylaeva@gmail.com>
2023-02-02 13:24:07 -0500

    Add some draw functions, not quite working yet

--- main.c
+++ main.c
@@ -3,12 +3,40 @@
 #include <GL/glut.h>
 #include "cpu.h"
 
+void draw() {
+    glClear(GLUT_SINGLE | GLUT_RGB);
+    glMatrixMode(GL_PROJECTION);    
+
+    for(int i = 0; i < 64; i++) {
+        for(int j = 0; j < 32; j++) {
+            glBegin(GL_POINTS);
+
+            if (display[i][j] == true) glColor3f(255, 255, 255);
+            else glColor3f(1, 1, 1);
+
+            glVertex2i(i, j);
+            glEnd();
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
+
+    for(int i = 0; i < 64; i++) {
+        for(int j = 0; j < 32; j++) {
+            if(i == j) {
+                display[i][j] = true;
+            } else display[i][j] = false;
+        }
+    }
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(64, 32);
+    glViewport(0, 0, 640, 320);
     glutCreateWindow("CHIP-8 Emulator");
+
+    draw();
 
     glutMainLoop();
 
